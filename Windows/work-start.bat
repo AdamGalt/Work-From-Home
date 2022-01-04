12b4741c6a86298d14e550e5c2b37adad8095b41
Adam Galt <AdamGalt@users.noreply.github.com>
2022-01-04 17:54:40 +0000

    Update work-start.bat

--- Windows/work-start.bat
+++ Windows/work-start.bat
@@ -4,16 +4,10 @@ rem --------------------------------------
 rem This automatically launches the applications you use for work.
 rem --------------------------------------
 
-rem Enter your teams location below or comment out.
-cd "C:\Program Files (x86)\Teams Installer\"
-start Teams.exe
-
-rem --------------------------------------
-
 rem If you need Caffiene https://www.zhornsoftware.co.uk/caffeine/#download
 rem Enter your location below or comment out.
-cd "C:\"
-start caffeine.exe
+cd "C:\Users\Adam\Apps\"
+start caffeine64.exe
 
 rem --------------------------------------
 
@@ -23,24 +17,23 @@ start Spotify.exe
 
 rem --------------------------------------
 
-rem Enter your Riot location below, or comment out.
-cd "C:\Users\adam\AppData\Local\riot-web\"
-start Riot.exe
-
-rem --------------------------------------
-
 rem Enter your email location below, or comment out.
-cd "C:\Program Files (x86)\Microsoft Office\root\Office16\"
+cd "C:\Program Files\Microsoft Office\root\Office16"
 start Outlook.exe
-
-rem --------------------------------------
-
-rem Enter your terminal location below, or comment out.
-cd "C:\Users\adam\AppData\Local\Programs\Termius"
-start Termius.exe
 
 rem --------------------------------------
 
 rem Enter your text editor location below, or comment out.
 cd "C:\Program Files (x86)\Notepad++\"
 start notepad++.exe
+
+rem -------------------------------------
+
+rem Launch Slack
+cd "C:\Users\Adam\AppData\Local\slack\"
+start slack.exe
+
+rem -------------------------------------
+
+rem Launch - Remote Desktop Connection
+mstsc

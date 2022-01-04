423f7d5b15df8cf7b2c51638505b61d07b4571f3
Adam Galt <AdamGalt@users.noreply.github.com>
2022-01-04 17:56:02 +0000

    Update work-stop.bat

--- Windows/work-stop.bat
+++ Windows/work-stop.bat
@@ -4,10 +4,9 @@ rem --------------------------------------
 rem This automatically stops the applications you use for work.
 rem --------------------------------------
 TASKKILL /F /IM teams.exe
-TASKKILL /F /IM caffeine.exe
-TASKKILL /F /IM riot.exe
+TASKKILL /F /IM caffeine64.exe
 TASKKILL /F /IM spotify.exe
 TASKKILL /F /IM outlook.exe
 TASKKILL /F /IM notepad++.exe
-
-rem NOTE: termius left open on purpose in case something is still running or being monitored.
+TASKKILL /F /IM slack.exe
+TASKKILL /F /IM mstsc.exe

c22de1ac6b81e56787f46d51f505f7677c3e6903
Adam Galt <AdamGalt@users.noreply.github.com>
2022-01-05 17:56:39 +0000

    Update work-start.bat

--- Windows/work-start.bat
+++ Windows/work-start.bat
@@ -6,34 +6,29 @@ rem --------------------------------------
 
 rem If you need Caffiene https://www.zhornsoftware.co.uk/caffeine/#download
 rem Enter your location below or comment out.
-cd "C:\Users\Adam\Apps\"
-start caffeine64.exe
+start "" "C:\Users\Adam\Apps\caffeine64.exe"
 
 rem --------------------------------------
 
 rem Replace this with your own music application or location or comment out.
-cd "C:\Users\adam\AppData\Roaming\Spotify\"
-start Spotify.exe
+start "" "C:\Users\adam\AppData\Roaming\Spotify\Spotify.exe"
 
 rem --------------------------------------
 
 rem Enter your email location below, or comment out.
-cd "C:\Program Files\Microsoft Office\root\Office16"
-start Outlook.exe
+start "" "C:\Program Files\Microsoft Office\root\Office16\Outlook.exe"
 
 rem --------------------------------------
 
 rem Enter your text editor location below, or comment out.
-cd "C:\Program Files (x86)\Notepad++\"
-start notepad++.exe
+start "" "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
 rem -------------------------------------
 
 rem Launch Slack
-cd "C:\Users\Adam\AppData\Local\slack\"
-start slack.exe
+start "" "C:\Users\Adam\AppData\Local\slack\slack.exe"
 
 rem -------------------------------------
 
 rem Launch - Remote Desktop Connection
-mstsc
+start "" "mstsc"
